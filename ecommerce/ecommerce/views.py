c6bfff194daa826db07b97e2d52c262789834953
unknown <nadim.dipu@northsouth.edu>
2021-03-20 20:36:44 +0600

    Working on the html contact form.

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -2,10 +2,25 @@ from django.shortcuts import HttpResponse, render
 
 
 def home_page(request):
-    return render(request, "ecommerce/home_page.html")
+    context = {
+        "title": "Hello World!",
+    }
+    return render(request, "ecommerce/home_page.html", context)
+
 
 def about_page(request):
-    return render(request, "ecommerce/about_page.html")
+    context = {
+        "title": "About",
+    }
+    return render(request, "ecommerce/about_page.html", context)
+
 
 def contact_page(request):
-    return render(request, "ecommerce/contact_page.html")
+    context = {
+        "title": "Contact",
+    }
+    if request.method == "POST":
+        print("A post request has been made!", request.POST.get('fullName'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
